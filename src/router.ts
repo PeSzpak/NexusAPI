0452dd76739d1fd09952cd077acff1dc95073713
Pedro Szpak <dropszpalves@gmail.com>
2025-07-03 11:15:42 -0300

    feat: create initial controller for resource handling

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router,Request, Response } from "express";
+import { createMovie } from "./controllers/movieControllers";
 const router = Router()
 
 export default router.get("/test", (req:Request, res:Response) => {
     res.status(200).send("API Working!");
-})
+}).post("/movie", createMovie)
